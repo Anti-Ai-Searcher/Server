a7fc8c0a599cacd34d74ce86b696146f4d045850
JaeChanHwang <hss598876@gmail.com>
2025-05-21 14:58:07 +0900

    Change detect_ai.py

--- detect_ai.py
+++ detect_ai.py
@@ -6,17 +6,7 @@ import torch
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
 import torch.nn.functional as F
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model_name = 'roberta-base'  # 학습할 때 사용한 모델이 'roberta-large'였는지 확인
-model = RobertaForSequenceClassification.from_pretrained(model_name)
-
-checkpoint = torch.load("best-model.pt", map_location=device, weights_only=True)
-tokenizer = RobertaTokenizer.from_pretrained(model_name)
-
-model.load_state_dict(checkpoint["model_state_dict"], strict=False)
-model.to(device)
-
-def detect_ai_generated_text(text):
+def detect_ai_generated_text(text : str,tokenizer , model, device):
     try:
         inputs = tokenizer(
             text, padding=True, truncation=True, max_length=128, return_tensors="pt"
@@ -27,6 +17,8 @@ def detect_ai_generated_text(text):
         
         logits = outputs.logits
         probabilities = F.softmax(logits, dim=1)
+        ai_probability = probabilities[:, 1].item()
+        print(ai_probability)
         ai_probability = probabilities[:, 0].item()
         #print(ai_probability)
 
