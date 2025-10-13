a5540df009b993a33d6fe9347b9fcc5d88253257
Jaechan Hwang <emperor0827@gmail.com>
2025-10-13 13:52:21 +0900

    Update Test and fix some funtions about Image
    1. add testfile for testing image
    2. update detecting image functions to check the probability
    3. add image model to conftest.py
    
    Signed-off-by: Jaechan Hwang <emperor0827@gmail.com>

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import torch
-from models import kor_loader, eng_loader
+from models import kor_loader, eng_loader, img_loader
 from models.model import device
 
 @pytest.fixture(scope="session")
@@ -9,9 +9,11 @@ def ai_models():
     
     model_kor, tokenizer_kor = kor_loader.get_korean_model()
     model_eng, tokenizer_eng = eng_loader.get_english_model()
-
+    model_img, model_img_preprocess = img_loader.get_image_model()
+    
     return {
         "kor": {"model": model_kor, "tokenizer": tokenizer_kor},
         "eng": {"model": model_eng, "tokenizer": tokenizer_eng},
+        "img": {"model": model_img, "preprocessor": model_img_preprocess},
         "device": device
     }
--- test.py
+++ test.py
@@ -2,21 +2,10 @@
 import app
 import read_contents.crawl as crawl
 from ai_detector import detector
-from models import kor_loader, eng_loader
-from models.model import device
+from PIL import Image
 
 # import the test client
 from fastapi.testclient import TestClient
-import pytest
-
-# AI module 
-import torch
-from transformers import RobertaTokenizer, RobertaForSequenceClassification, AutoTokenizer
-import torch.nn.functional as F
-from models.model import TransformerClassifier
-
-####
-
 
 # Create a test client using the FastAPI app
 client = TestClient(app.app)
@@ -32,7 +21,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("engAI1",result)
         assert result > 0.7
     def test_AI_texts_english_2(self, ai_models):
         ai_text_link = "https://velog.io/@aiyaho123/Dyson-V12-Detect-Slim-Review-A-Laser-Powered-Cleaning-Experience-for-the-Data-Oriented-Mind#-final-thoughts"
@@ -42,7 +31,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("engAI2",result)
         assert result > 0.7
     def test_AI_texts_english_3(self, ai_models):
         ai_text_link = "https://velog.io/@aiyaho123/Prison-Architect-Review-A-Simulation-Sandbox-for-Systems-Thinkers"
@@ -52,7 +41,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("engAI3",result)
         assert result > 0.7
     def test_AI_texts_english_4(self, ai_models):
         ai_text_link = "https://velog.io/@aiyaho123/Spaghetti-Bolognese-for-Developers"
@@ -62,7 +51,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("engAI4",result)
         assert result > 0.7
     
     def test_human_texts_english_1(self, ai_models):
@@ -73,7 +62,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("enghum1",result)
         assert result < 0.4
     def test_human_texts_english_2(self, ai_models):
         human_text_link = "https://www.instructables.com/How-to-install-Linux-on-your-Windows/"
@@ -83,7 +72,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("enghum2",result)
         assert result < 0.4
     def test_human_texts_english_3(self, ai_models):
         human_text_link = "https://medium.com/@ivan.mejia/c-development-using-visual-studio-code-cmake-and-lldb-d0f13d38c563"
@@ -93,7 +82,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("enghum3",result)
         assert result < 0.4
     def test_human_texts_english_4(self, ai_models):
         human_text_link = "https://medium.com/@rjun07a/my-spring-2016-anime-list-23a226b2bc14"
@@ -103,7 +92,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("enghum4",result)
         assert result < 0.4
 
     # Test for the korean text AI detection 
@@ -115,7 +104,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("koAI1",result)
         assert result > 0.7
     def test_AI_texts_korean_2(self, ai_models):
         ai_text_link = "https://ai3886.tistory.com/3"
@@ -125,7 +114,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("koAI2",result)
         assert result > 0.7
     def test_AI_texts_korean_3(self, ai_models):
         ai_text_link = "https://ai3886.tistory.com/4"
@@ -135,7 +124,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("koAI3",result)
         assert result > 0.7
     def test_AI_texts_korean_4(self, ai_models):
         ai_text_link = "https://ai3886.tistory.com/5"
@@ -145,7 +134,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("koAI4",result)
         assert result > 0.7
     
     def test_human_texts_korean_1(self, ai_models):
@@ -156,7 +145,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("kohum1",result)
         assert result < 0.4
     def test_human_texts_korean_2(self, ai_models):
         human_text_link = "https://aboooks.tistory.com/37"
@@ -166,7 +155,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("kohum2",result)
         assert result < 0.4
     def test_human_texts_korean_3(self, ai_models):
         human_text_link = "https://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0000304103"
@@ -176,7 +165,7 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("kohum3",result)
         assert result < 0.4
     def test_human_texts_korean_4(self, ai_models):
         human_text_link = "https://m.blog.naver.com/junkigi11/20173492987"
@@ -186,5 +175,41 @@ class TestDetectAI:
             ai_models["eng"]["model"],
             ai_models["kor"]["model"],
         )
-        print(result)
+        print("kohum4",result)
         assert result < 0.4
+
+    def test_human_image_1(self, ai_models):
+        img_path = "./testfile/IU.jpeg"
+        img = Image.open(img_path).convert("RGB")
+
+        ai_prob = detector.detect_ai_generated_image(img,ai_models["img"]["model"])
+
+        print("imghum1",ai_prob)
+        assert ai_prob < 0.6
+    
+    def test_human_image_2(self, ai_models):
+        img_path = "./testfile/street.jpg"
+        img = Image.open(img_path).convert("RGB")
+
+        ai_prob = detector.detect_ai_generated_image(img,ai_models["img"]["model"])
+
+        print("imghum2",ai_prob)
+        assert ai_prob < 0.6
+    
+    def test_ai_image_1(self, ai_models):
+        img_path = "./testfile/midjourney.jpeg"
+        img = Image.open(img_path).convert("RGB")
+
+        ai_prob = detector.detect_ai_generated_image(img,ai_models["img"]["model"])
+
+        print("imgAI1",ai_prob)
+        assert ai_prob >= 0.6
+    
+    def test_ai_image_2(self, ai_models):
+        img_path = "./testfile/nanobanana.png"
+        img = Image.open(img_path).convert("RGB")
+
+        ai_prob = detector.detect_ai_generated_image(img,ai_models["img"]["model"])
+
+        print("imgAI2",ai_prob)
+        assert ai_prob >= 0.6
