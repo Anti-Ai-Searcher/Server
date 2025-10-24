e374377b37064b89ffd9806569574f87e9b8cb2e
dudcks <dudcks3886@naver.com>
2025-10-24 13:12:33 +0900

    영어 모델 로드 로그출력 수정

--- models/eng_loader.py
+++ models/eng_loader.py
@@ -1,16 +1,19 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from models.model import device
+from transformers.utils import logging as hf_logging
+
+hf_logging.set_verbosity_error()
 
 def get_english_model():
     print("Load English Model")
-    ckpt = torch.load('ai_model/model_eng.pt', map_location=device)
+    ckpt = torch.load('ai_model/model_eng.pt', map_location=device, weights_only=True)
     model_name = ckpt.get('model_name', 'microsoft/deberta-v3-base')
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels = 2)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels = 2, force_download=False)
     state = ckpt['state_dict'] if 'state_dict' in ckpt else ckpt
     model.load_state_dict(state, strict = True)
     model.to(device)
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast = False)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast = False, force_download=False)
     model.eval()
     print("Done")
     return model, tokenizer
